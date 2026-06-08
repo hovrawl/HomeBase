4bf49aa2911493c2bf04ba79e728020cbfe2b2b4
NahFarn <nathan.singh@inreachsw.com>
2026-06-08 10:53:07 +1000

    Improve icons to use win32

--- WinDock/Program.cs
+++ WinDock/Program.cs
@@ -1,11 +1,14 @@
 ﻿using System.Numerics;
+using System.Runtime.InteropServices;
+using Windows.Win32;
 using Silk.NET.Input;
 using Silk.NET.Maths;
 using Silk.NET.Windowing;
-using Silk.NET.Windowing.Glfw;
 using SkiaSharp;
 using Windows.Win32.Foundation;
-using Silk.NET.Direct2D;
+using Windows.Win32.Graphics.Gdi;
+using Windows.Win32.System.Com;
+using Windows.Win32.UI.Shell;
 using WinDock;
 using WindowState = Silk.NET.Windowing.WindowState;
 
@@ -15,8 +18,9 @@ HWND hwnd = HWND.Null;
 GRBackendRenderTarget? skiaRenderTarget = null;
 SKSurface? skiaSurface = null;
 Vector2D<int> lastFramebufferSize = new(0, 0);
-
 IInputContext WindowInput = null;
+
+Dictionary<string, SKImage> _iconCache = new(StringComparer.OrdinalIgnoreCase);
 
 // Mouse Globals
 Vector2 MousePosition = new Vector2(0, 0);
@@ -98,6 +102,13 @@ GlobalHotkey.Start(() =>
 window.Run();
 
 TrayIcon.Remove();
+
+foreach (SKImage iconImage in _iconCache.Values)
+{
+    iconImage.Dispose();
+}
+
+_iconCache.Clear();
 
 skiaRenderTarget?.Dispose();
 skiaSurface?.Dispose();
@@ -502,6 +513,22 @@ bool DockItemButton(SKCanvas canvas, SKRect rect, StorageService.DockItem item, 
 
     canvas.DrawRoundRect(rect, 12, 12, cardPaint);
 
+    SKImage? icon = GetDockItemIcon(item.Path);
+
+    if (icon is not null)
+    {
+        // icon rect is inner rect with small padding
+        var iconPadding = 1;
+        SKRect iconRect = new SKRect(
+            rect.Left + iconPadding,
+            rect.Top + iconPadding,
+            rect.Right - iconPadding,
+            rect.Bottom - iconPadding
+        );
+        
+        canvas.DrawImage(icon, iconRect);
+    }
+    
     if (active && LeftMouseReleased)
     {
         ActiveElementId = null;
@@ -604,6 +631,171 @@ void DismissDock()
     window.Position = new Vector2D<int>(centerX - window.Size.X / 2, bottomY + window.Size.Y);
 }
 
+SKImage? GetDockItemIcon(string path)
+{
+    if (_iconCache.TryGetValue(path, out SKImage? image))
+    {
+        return image;
+    }
+
+    image = LoadIconImageForFile(path);
+    
+
+    if (image is not null)
+    {
+        _iconCache[path] = image;
+    }
+
+    return image;
+}
+
+unsafe SKImage? LoadIconImageForFile(string path)
+{
+    // load image from filepath
+    if (string.IsNullOrWhiteSpace(path))
+    {
+        return null;
+    }
+
+    const int requestedIconSize = 128;
+
+    HRESULT hr = PInvoke.CoInitializeEx(
+        null,
+        COINIT.COINIT_APARTMENTTHREADED | COINIT.COINIT_DISABLE_OLE1DDE);
+
+    bool shouldUninitialize = hr.Succeeded;
+
+    HBITMAP bitmapHandle = HBITMAP.Null;
+
+    try
+    {
+        Guid shellItemImageFactoryId = typeof(IShellItemImageFactory).GUID;
+
+        hr = PInvoke.SHCreateItemFromParsingName(
+            path,
+            null,
+            in shellItemImageFactoryId,
+            out object imageFactoryObject);
+
+        if (hr.Failed || imageFactoryObject is not IShellItemImageFactory imageFactory)
+        {
+            return null;
+        }
+
+        SIZE size = new()
+        {
+            cx = requestedIconSize,
+            cy = requestedIconSize
+        };
+
+        try
+        {
+            imageFactory.GetImage(
+                size,
+                SIIGBF.SIIGBF_ICONONLY | SIIGBF.SIIGBF_BIGGERSIZEOK,
+                &bitmapHandle);
+        }
+        catch
+        {
+            return null;
+        }
+
+        if (bitmapHandle == HBITMAP.Null)
+        {
+            return null;
+        }
+
+        return CreateSkImageFromHBitmap(bitmapHandle);
+    }
+    finally
+    {
+        if (bitmapHandle != HBITMAP.Null)
+        {
+            _ = PInvoke.DeleteObject(bitmapHandle);
+        }
+
+        if (shouldUninitialize)
+        {
+            PInvoke.CoUninitialize();
+        }
+    }
+}
+
+unsafe SKImage? CreateSkImageFromHBitmap(HBITMAP bitmapHandle)
+{
+    BITMAP bitmap = default;
+
+    int objectSize = PInvoke.GetObject(
+        new HGDIOBJ(bitmapHandle.Value),
+        sizeof(BITMAP),
+        &bitmap);
+
+    if (objectSize == 0 || bitmap.bmWidth <= 0 || bitmap.bmHeight <= 0)
+    {
+        return null;
+    }
+
+    int width = bitmap.bmWidth;
+    int height = bitmap.bmHeight;
+
+    BITMAPINFO bitmapInfo = default;
+    bitmapInfo.bmiHeader.biSize = (uint)sizeof(BITMAPINFOHEADER);
+    bitmapInfo.bmiHeader.biWidth = width;
+    bitmapInfo.bmiHeader.biHeight = -height;
+    bitmapInfo.bmiHeader.biPlanes = 1;
+    bitmapInfo.bmiHeader.biBitCount = 32;
+    bitmapInfo.bmiHeader.biCompression = 0;
+
+    int stride = width * 4;
+    int byteCount = stride * height;
+    byte[] pixels = new byte[byteCount];
+
+    HDC screenDc = PInvoke.GetDC(HWND.Null);
+
+    try
+    {
+        fixed (byte* pixelsPtr = pixels)
+        {
+            int scanLines = PInvoke.GetDIBits(
+                screenDc,
+                bitmapHandle,
+                0,
+                (uint)height,
+                pixelsPtr,
+                &bitmapInfo,
+                DIB_USAGE.DIB_RGB_COLORS);
+
+            if (scanLines == 0)
+            {
+                return null;
+            }
+        }
+    }
+    finally
+    {
+        _ = PInvoke.ReleaseDC(HWND.Null, screenDc);
+    }
+
+    SKImageInfo imageInfo = new(
+        width,
+        height,
+        SKColorType.Bgra8888,
+        SKAlphaType.Premul);
+
+    using SKBitmap skBitmap = new(imageInfo);
+
+    nint skiaPixels = skBitmap.GetPixels();
+
+    if (skiaPixels == 0)
+    {
+        return null;
+    }
+
+    Marshal.Copy(pixels, 0, skiaPixels, byteCount);
+
+    return SKImage.FromBitmap(skBitmap);
+}
+
 void OnDockItemClicked(StorageService.DockItem dockItem)
 {
     // click dock item
@@ -617,5 +809,82 @@ void OpenAddItemDialog()
     // show file picker dialog
     Console.WriteLine("Add Pressed");
     
+    var result = OpenFilePicker(HWND.Null);
+    if (result == null)
+        return;
+    
+    var dockItem = new StorageService.DockItem(result.Item1, result.Item2);
+    Console.WriteLine($"Selected file: {dockItem.Name}, Path: {dockItem.Path}");
+    
+    DockItems.Add(dockItem);
+}
+
+static unsafe Tuple<string,string>? OpenFilePicker(HWND owner)
+{
+    // COM must be initialized on the thread that shows the dialog.
+    HRESULT hr = PInvoke.CoInitializeEx(
+        null,
+        COINIT.COINIT_APARTMENTTHREADED | COINIT.COINIT_DISABLE_OLE1DDE);
+
+    bool shouldUninitialize = hr.Succeeded;
+
+    try
+    {
+        IFileOpenDialog dialog = FileOpenDialog.CreateInstance<IFileOpenDialog>();
+
+        // Optional: set title
+        dialog.SetTitle("Choose a file");
+
+        // Optional: only allow existing files
+        FILEOPENDIALOGOPTIONS options;
+        dialog.GetOptions(&options);
+        dialog.SetOptions(options | FILEOPENDIALOGOPTIONS.FOS_FILEMUSTEXIST);
+
+        try
+        {
+            dialog.Show(owner);
+        }
+        catch (Exception ex)
+        {
+            // cancelled by user
+            return null;
+        }
+
+        // // User cancelled
+        // if (hr == HRESULT_FROM_WIN32(WIN32_ERROR.ERROR_CANCELLED))
+        // {
+        //     return null;
+        // }
+        //
+        // hr.ThrowOnFailure();
+
+        dialog.GetResult(out IShellItem result);
+
+        PWSTR pathPtr;
+        PWSTR fileNmPtr;
+        result.GetDisplayName(SIGDN.SIGDN_FILESYSPATH, &pathPtr);
+        result.GetDisplayName(SIGDN.SIGDN_NORMALDISPLAY, &fileNmPtr);
+        try
+        {
+            return new Tuple<string, string>(fileNmPtr.ToString(), pathPtr.ToString());
+        }
+        finally
+        {
+            PInvoke.CoTaskMemFree(pathPtr);
+            PInvoke.CoTaskMemFree(fileNmPtr);
+        }
+    }
+    finally
+    {
+        if (shouldUninitialize)
+        {
+            PInvoke.CoUninitialize();
+        }
+    }
+
+    // static HRESULT HRESULT_FROM_WIN32(WIN32_ERROR error)
+    // {
+    //     return (HRESULT)(int)(0x80070000u | (uint)error);
+    // }
 }
 #endregion
